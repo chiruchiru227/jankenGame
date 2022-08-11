e33f81f500f0e26eb8d44729570be3013154f879
michiruKajitsuka <chiruchirumichiru227@gmail.com>
2022-08-11 19:26:50 +0900

    main.javaを新規クラスに分割

--- src/main/java/originalGame/janken/Main.java
+++ src/main/java/originalGame/janken/Main.java
@@ -3,7 +3,6 @@ package main.java.originalGame.janken;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 
@@ -29,29 +28,29 @@ public class Main {
 		frame.setResizable(false);
 		
 		//ヘッダーパネルを作成
-		JPanel headerPanel = makePanel(Color.BLACK, 640, 50);
+		JPanel headerPanel = PanelMaker.makePanel(Color.BLACK, 640, 50);
 		//ヘッダーパネルに、レイアウトマネージャとしてBorderLayoutを指定
 		headerPanel.setLayout(new BorderLayout());
 		//テキストを作成
-		JLabel headerLabel = makeText("「さあ、じゃんけんで勝負だ！」", 24, Color.WHITE);
+		JLabel headerLabel = TextMaker.makeText("「さあ、じゃんけんで勝負だ！」", 24, Color.WHITE);
 		//ヘッダーパネルにテキストを追加
 		headerPanel.add(headerLabel);
 		//ウィンドウにヘッダーパネルを追加
 		frame.add(headerPanel, BorderLayout.NORTH);
 		
 		//コンテンツパネルを作成
-		JPanel contentsPanel = makePanel(Color.WHITE);
+		JPanel contentsPanel = PanelMaker.makePanel(Color.WHITE);
 		//コンテンツパネルに、レイアウトマネージャとしてBorderLayoutを指定
 		contentsPanel.setLayout(new BorderLayout());
 		//テキストを作成
-		JLabel contentsLabel = makeText("じゃんけん……", 54, Color.BLACK);
+		JLabel contentsLabel = TextMaker.makeText("じゃんけん……", 54, Color.BLACK);
 		//コンテンツパネルにテキストを追加
 		contentsPanel.add(contentsLabel);
 		//ウィンドウにコンテンツパネルを追加
 		frame.add(contentsPanel, BorderLayout.CENTER);
 		
 		//フッターパネルを作成
-		JPanel footerPanel = makePanel(Color.BLACK, 640, 50);
+		JPanel footerPanel = PanelMaker.makePanel(Color.BLACK, 640, 50);
 		//フッターパネルに、レイアウトマネージャとしてGridLayoutを指定
 		footerPanel.setLayout(new GridLayout());
 
@@ -72,42 +71,6 @@ public class Main {
 		frame.setVisible(true);
 	}
 	
-	//パネルを作るメソッド（幅と高さを指定する用）
-	static JPanel makePanel(Color color, int width, int height) {
-		//JPanelクラスをインスタンス化
-		JPanel panel = new JPanel();
-		//パネルの色を変更する
-		panel.setBackground(color);
-		//パネルのサイズを「width、height」に設定
-		panel.setPreferredSize(new Dimension(width, height));
-		//作ったパネルを返す
-		return panel;
-	}
-	
-	//パネルを作るメソッド（幅と高さを指定しない用）
-	static JPanel makePanel(Color color) {
-		//JPanelクラスをインスタンス化
-		JPanel panel = new JPanel();
-		//パネルの色を変更する
-		panel.setBackground(color);
-		//作ったパネルを返す
-		return panel;
-	}
-	
-	//テキストを作るメソッド
-	static JLabel makeText(String str, int size, Color color) {
-		//JLabelクラスをインスタンス化
-		JLabel label = new JLabel(str);
-		//テキストの文字を白にする
-		label.setForeground(color);
-		//テキストのフォントを設定
-		label.setFont(new Font("ＭＳ ゴシック", Font.PLAIN, size));
-		//テキストの位置を、水平方向の中心にする
-		label.setHorizontalAlignment(JLabel.CENTER);
-		//テキストの位置を、垂直方向の中心にする
-		label.setVerticalAlignment(JLabel.CENTER);
-		//作ったテキストを返す
-		return label;
-	}
+
 
 }
